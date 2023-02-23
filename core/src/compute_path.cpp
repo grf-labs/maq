83cb0ba9cf0b738a7d5a08f7c473284958749631
Erik <erik.sverdrup@gmail.com>
2023-02-22 17:52:48 -0800

    wp

--- core/src/compute_path.cpp
+++ core/src/compute_path.cpp
@@ -51,10 +51,8 @@ solution_path compute_path(const std::vector<size_t>& samples,
                            double budget,
                            bool bootstrap) {
   std::vector<std::vector<double>> spend_gain(3); // 3rd entry: SEs
-  std::vector<std::vector<size_t>> i_k_path(2);
-
+  std::vector<std::vector<size_t>> i_k_path(3); // 3rd entry: complete path?
   std::vector<size_t> active_set(data.num_rows, 0); // active R entry offset by one.
-  // std::unordered_map<size_t, size_t> active_set; // slower
 
   // Initialize PQ with initial enrollment
   std::priority_queue<QueueElement> pqueue;
