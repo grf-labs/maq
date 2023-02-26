7b069d03da02eab78d720d2e73e34c4aa5c9c0b8
Erik <erik.sverdrup@gmail.com>
2023-02-26 09:41:48 -0800

    ws

--- python-package/maq/maq.py
+++ python-package/maq/maq.py
@@ -2,16 +2,17 @@ import numpy as np
 
 from maq.ext import solver_cpp
 
+
 class MAQ:
-  r"""
-  abc etc
-  """
+    r"""
+    abc etc
+    """
 
-  def __init__(self, num_bootstrap=200, seed=42):
-    self.num_bootstrap = num_bootstrap
-    self.seed = seed
+    def __init__(self, num_bootstrap=200, seed=42):
+        self.num_bootstrap = num_bootstrap
+        self.seed = seed
 
-  def fit(reward, cost, budget):
-    #etc
+    def fit(reward, cost, budget):
+        # etc
 
-    return self
+        return self
