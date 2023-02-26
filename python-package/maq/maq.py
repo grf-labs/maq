fad327b20087bfb38780b34035f7d0439d52f72d
Erik <erik.sverdrup@gmail.com>
2023-02-26 09:38:16 -0800

    Revert "wp"
    This reverts commit c4b7dbce893b23f0de294fe2c8541596e4a40fa0.

--- python-package/maq/maq.py
+++ python-package/maq/maq.py
@@ -3,15 +3,15 @@ import numpy as np
 from maq.ext import solver_cpp
 
 class MAQ:
-    r"""
-    abc etc
-    """
+  r"""
+  abc etc
+  """
 
   def __init__(self, num_bootstrap=200, seed=42):
-      self.num_bootstrap = num_bootstrap
-      self.seed = seed
+    self.num_bootstrap = num_bootstrap
+    self.seed = seed
 
   def fit(reward, cost, budget):
-      #etc
+    #etc
 
-      return self
+    return self
