24daba45258439f1bfb17235599024e89e6b4882
Erik <erik.sverdrup@gmail.com>
2023-02-26 14:48:00 -0800

    wp

--- python-package/maq/maq.py
+++ python-package/maq/maq.py
@@ -11,8 +11,8 @@ class MAQ:
     """
 
     def __init__(self, n_bootstrap=200, n_threads=0, seed=42):
-        assert n_threads >= 0
-        assert n_bootstrap >= 0
+        assert n_threads >= 0, "n_threads should be >=0."
+        assert n_bootstrap >= 0, "n_bootstrap should be >=0."
         self.n_bootstrap = n_bootstrap
         self.n_threads = n_threads
         self.seed = seed
@@ -21,11 +21,11 @@ class MAQ:
     def fit(self, reward, cost, budget):
         reward = np.atleast_2d(reward)
         cost = np.atleast_2d(cost)
-        assert reward.shape == cost.shape
-        assert np.isscalar(budget)
-        assert (cost > 0).all()
-        assert not np.isnan(reward).any()
-        assert not np.isnan(cost).any()
+        assert reward.shape == cost.shape, "reward and cost should have equal dims."
+        assert np.isscalar(budget), "budget should be a scalar."
+        assert (cost > 0).all(), "cost should be > 0."
+        assert not np.isnan(reward).any(), "reward contains nans."
+        assert not np.isnan(cost).any(), "cost contains nans."
 
         self._path = solver_cpp(reward, cost, budget,
             self.n_bootstrap, self.n_threads, self.seed)
