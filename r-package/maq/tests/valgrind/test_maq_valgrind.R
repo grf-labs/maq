effa4d1f7a0945f3a47fbc590c354828b4e4ca57
Erik <erik.sverdrup@gmail.com>
2023-03-02 18:47:17 -0800

    valgrind

--- r-package/maq/tests/valgrind/test_maq_valgrind.R
+++ r-package/maq/tests/valgrind/test_maq_valgrind.R
@@ -6,8 +6,9 @@ budget <- 1000
 n <- 1500
 K <- 10
 reward <- matrix(0.1 + rnorm(n * K), n, K)
+reward.eval <- matrix(0.1 + rnorm(n * K), n, K)
 cost <- 0.05 + matrix(runif(n * K), n, K)
 wts <- runif(n)
 clust <- sample(1:250, n, TRUE)
 
-mq <- maq(reward, cost, budget, sample.weights = wts, clusters = clust)
+mq <- maq(reward, cost, budget, reward.eval, sample.weights = wts, clusters = clust)
