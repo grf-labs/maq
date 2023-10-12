adff0e578c65ac16a49a8d2df097e77698b71f8a
Erik <erik.sverdrup@gmail.com>
2023-10-12 14:14:21 -0700

    -10

--- python-package/tests/test_maq.py
+++ python-package/tests/test_maq.py
@@ -242,8 +242,8 @@ def test_integrated_difference_grid_numerics():
         est1 = (g1 - g2) / (i + 1)
         est2 = np.mean(q1.path_gain_[: i + 1]) - np.mean(q2.path_gain_[: i + 1])
         est = q1.integrated_difference(q2, spend)[0]
-        nt.assert_allclose(est1, est2, rtol=1e-07)
-        nt.assert_allclose(est, est2, rtol=1e-07)
+        nt.assert_allclose(est1, est2, rtol=1e-10)
+        nt.assert_allclose(est, est2, rtol=1e-10)
 
 
 def test_basic_perf_timing():
