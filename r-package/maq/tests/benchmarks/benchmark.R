9e8dda3275183bc8c1a20b903c20cc93ff999b12
Erik Sverdrup <erik.sverdrup@gmail.com>
2024-02-16 13:16:07 -0800

    Add RELEASES.md (#69)

--- r-package/maq/tests/benchmarks/benchmark.R
+++ r-package/maq/tests/benchmarks/benchmark.R
@@ -3,13 +3,6 @@ library(maq)
 rm(list = ls())
 set.seed(42)
 
-sessionInfo()
-# R version 4.2.2 (2022-10-31)
-# Platform: x86_64-apple-darwin17.0 (64-bit)
-# Running under: macOS Monterey 12.6.3
-parallel::detectCores()
-# [1] 8
-
 # 1 medium n medium K
 n = 1e5
 K = 10
@@ -83,35 +76,3 @@ print(b41, digits = 3)
 print(b42, digits = 3)
 print(b5, digits = 3)
 print(list(b1,b2,b3,b41,b42,b5), digits = 3)
-
-# v0.1.0
-# > print(list(b1,b2,b3,b41,b42,b5), digits = 3)
-# [[1]]
-# Unit: seconds
-# expr  min   lq mean median   uq  max neval
-# maq(reward, cost, reward, R = 200, paired.inference = FALSE) 1.93 1.97    2   1.99 2.02 2.12    10
-#
-# [[2]]
-# Unit: seconds
-# expr  min   lq mean median   uq  max neval
-# maq(reward, cost, reward, R = 200, paired.inference = FALSE) 3.84 3.85 3.87   3.87 3.89 3.92    10
-#
-# [[3]]
-# Unit: seconds
-# expr  min   lq mean median   uq  max neval
-# maq(reward, cost, reward, R = 200, paired.inference = FALSE) 15.3 15.3 15.4   15.3 15.3 15.8     5
-#
-# [[4]]
-# Unit: seconds
-# expr  min   lq mean median   uq  max neval
-# maq(reward, cost, reward, R = 0, paired.inference = FALSE) 1.91 1.92 1.94   1.93 1.95 2.01    10
-#
-# [[5]]
-# Unit: seconds
-# expr  min   lq mean median   uq  max neval
-# maq(reward, cost, reward, R = 200, paired.inference = FALSE) 31.7 31.8 32.9   32.1 32.4 40.7    10
-#
-# [[6]]
-# Unit: seconds
-# expr  min   lq mean median   uq  max neval
-# maq(reward, cost, reward, R = 0, paired.inference = FALSE) 10.2 10.2 10.2   10.2 10.2 10.2     5
